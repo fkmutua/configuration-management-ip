62829d4fd9e28fd023438678d9f0e97572f9e786
fkmutua <felixmutua75@gmail.com>
2020-11-12 11:37:26 -0800

    added terraform for resouce provisioning

---
+++ app.tf
@@ -0,0 +1,15 @@
+#Variables
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-west-1"
+}
+
+#Provisioning
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
